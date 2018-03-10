28caa3c44fa9d4a31e68023095ebbd93a923fc07
shavaju@gmail.com <shavaju@gmail.com>
2018-03-10 13:21:53 -0500

    added another print statement :)

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -4,6 +4,7 @@ public class GoodEvening {
 	public static void main(String[] args) {
 		System.out.println("Good Evening!");
 		System.out.println("Good Evening Katerina");
+		System.out.println("Good Evening Ekaterina and Nijat! :)");
 	}
 
 }
