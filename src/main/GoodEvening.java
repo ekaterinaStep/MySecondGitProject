c32b427757848052acfc66fa17f4cebacca937f3
aliyevnijat <aliyevnicat@gmail.com>
2018-03-10 14:36:54 -0500

    for loop added

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -5,6 +5,11 @@ public class GoodEvening {
 		System.out.println("Good Evening!");
 		System.out.println("Good Evening Katerina");
 		System.out.println("Good Evening Ekaterina and Nijat! :)");
+		System.out.println("===========================================");
+		System.out.println("I added for loop");
+		for (int i = 0; i < args.length; i++) {
+			
+		}
 	}
 
 }
