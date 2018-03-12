f692ace4fb2d858f92896c76adc3cd6bd07ddb67
aliyevnijat <aliyevnicat@gmail.com>
2018-03-11 23:02:27 -0400

    Another one

--- src/main/GoodNight.java
+++ src/main/GoodNight.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("good night");
 	System.out.println("Good Night ");
 	System.out.println("Another Try");
+	System.out.println("One more time");
 }
 }
